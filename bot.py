1d77061b52627cd6db2bd7263fd9386cb756441b
hodsonus <jhodson@ufl.edu>
2017-10-21 01:47:33 -0500

    fix reddit instantiation

--- bot.py
+++ bot.py
@@ -5,12 +5,15 @@ import praw
 #username: FootballStatsBot
 #password: Vandyhacksf17
 
-r = praw.Reddit(user_agent= 'FootballStatsBot prototype (by /u/hodsonus and /u/xMutations)')
+r = praw.Reddit(client_id='rfYN_lq0etP5xg',
+                     client_secret='y8IvOQA3KZrwAr0Qhk9e7E3mSn4',
+                     user_agent='FootballStatsBot prototype (by /u/hodsonus and /u/xMutations)',
+                     username='FootballStatsBot',
+                     password='Vandyhacksf17')
 
-r.login('FootballStatsBot', 'Vandyhacksf17')
+
+# r.login('FootballStatsBot', 'Vandyhacksf17')
 
 submissions = r.get_subreddit('news').get_top(limit=10)
 
-print submissions
-
-# subreddit = redditInstance.()
+print("submissions")
