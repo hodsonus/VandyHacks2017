6560ef51946b830e531f15eebf14b347f9627a29
hodsonus <jhodson@ufl.edu>
2017-10-21 10:47:53 -0500

    add comment scanning and better documentation

--- bot.py
+++ bot.py
@@ -1,22 +1,32 @@
 import praw
 
+#client id: rfYN_lq0etP5xg
 #secret: y8IvOQA3KZrwAr0Qhk9e7E3mSn4
-#name: FootballStatsBot
 #username: FootballStatsBot
 #password: Vandyhacksf17
 
+#create reddt instance that has permission to write
 reddit = praw.Reddit(client_id='rfYN_lq0etP5xg',
                      client_secret='y8IvOQA3KZrwAr0Qhk9e7E3mSn4',
                      user_agent='FootballStatsBot prototype (by /u/hodsonus and /u/xMutations)',
                      username='FootballStatsBot',
                      password='Vandyhacksf17')
 
-#prints false because we have permission to write
-print(reddit.read_only)
+#subreddit that we will be operating in
+subr = reddit.subreddit('fakeid')
 
-subr = reddit.subreddit('learnpython')
+#for a submission, subm, in the subreddit we are operating in. look at 'limit' amount of hot submissions.
+for subm in subr.hot(limit=1):
 
-# print(subr.description)
+    print("Title: ", subm.title)
+    print("Text: ", subm.selftext)
+    print("Score: ", subm.score)
+    print("---------------------------------\n")
 
-# for submission in reddit.subreddit('learnpython').hot(limit=10):
-#     print(submission.title)
+    #remove all instances of 'MoreComments' in the CommentForest
+    subm.comments.replace_more(limit=0)
+
+    #scan over each comment instance in the CommentForest
+    for comm in subm.comments.list():
+        print(comm.body, "\n")
+        comm.body.find("retard")
