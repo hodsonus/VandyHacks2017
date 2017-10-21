a1a0ac353c50db078e87a5d66e0156373f121ff1
hodsonus <jhodson@ufl.edu>
2017-10-21 01:17:01 -0500

    create reddit instance and login

---
+++ bot.py
@@ -0,0 +1,12 @@
+import praw
+
+redditInstance = praw.Reddit(user_agent= 'FootballStatsBot prototype (by /u/hodsonus and /u/xMutations)')
+
+# subreddit = redditInstance.()
+
+redditInstance.login('FootballStatsBot', 'Vandyhacksf17')
+
+#secret: y8IvOQA3KZrwAr0Qhk9e7E3mSn4
+#name: FootballStatsBot
+#username: FootballStatsBot
+#password: Vandyhacksf17
