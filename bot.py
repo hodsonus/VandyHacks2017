762d64878b16c05acefdea43fb75883201fada69
hodsonus <jhodson@ufl.edu>
2017-10-21 17:16:31 -0500

    added protection such that the same comment is not posted on more than once

--- bot.py
+++ bot.py
@@ -1,4 +1,7 @@
 import praw
+import os
+import pdb
+import re
 import nflgame
 import nflgame.player
 
@@ -17,13 +20,11 @@ reddit = praw.Reddit(client_id='rfYN_lq0etP5xg',
 #subreddit that we will be operating in
 subr = reddit.subreddit('FBStatsTestingGrounds')
 
+commented = open("commented.txt", "r+")
+lines = commented.readlines()
+
 #for a submission, subm, in the subreddit we are operating in. look at 'limit' amount of hot submissions.
 for subm in subr.new(limit=None):
-
-    # print("Title: ", subm.title)
-    # print("Text: ", subm.selftext)
-    # print("Score: ", subm.score)
-    # print("---------------------------------\n")
 
     #remove all instances of 'MoreComments' in the CommentForest
     subm.comments.replace_more(limit=0)
@@ -31,25 +32,27 @@ for subm in subr.new(limit=None):
     #scan over each comment instance in the CommentForest
     for comm in subm.comments.list():
 
-        # print(comm.body, "\n")
-
         callIndex = comm.body.find("!FootballStats")
-        if callIndex >= 0:
+
+        found = False
+        for line in lines:
+            if line.startswith(comm.id):
+                found = True
+                break
+
+        if (callIndex >= 0) and not found:
             yearStart = -1
             playerStart = callIndex + 15
+
             for i in range (len(comm.body)):
                 if comm.body[i].isdigit():
                     yearStart = i
                     break
+
             player = comm.body[playerStart:yearStart-1]
             year = int(comm.body[yearStart:])
-
-
             playerObj = nflgame.find(player, team=None)[0]
-            print(playerObj, ":", year)
-            print(playerObj.stats(year, week=None).formatted_stats())
-            print()
-
-
-        else:
-            print("not found")
+            response = playerObj.name + ": " + str(year) + "\n\n" + playerObj.stats(year, week=None).formatted_stats()
+            comm.reply(response)
+            commented.write(comm.id)
+            commented.write("\n")
