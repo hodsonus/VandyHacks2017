253dd733505be846f0fc8071cbaa725f0eccb9a0
hodsonus <jhodson@ufl.edu>
2017-10-21 01:33:09 -0500

    get submissions and print

--- bot.py
+++ bot.py
@@ -1,12 +1,16 @@
 import praw
 
-redditInstance = praw.Reddit(user_agent= 'FootballStatsBot prototype (by /u/hodsonus and /u/xMutations)')
-
-# subreddit = redditInstance.()
-
-redditInstance.login('FootballStatsBot', 'Vandyhacksf17')
-
 #secret: y8IvOQA3KZrwAr0Qhk9e7E3mSn4
 #name: FootballStatsBot
 #username: FootballStatsBot
 #password: Vandyhacksf17
+
+r = praw.Reddit(user_agent= 'FootballStatsBot prototype (by /u/hodsonus and /u/xMutations)')
+
+r.login('FootballStatsBot', 'Vandyhacksf17')
+
+submissions = r.get_subreddit('news').get_top(limit=10)
+
+print submissions
+
+# subreddit = redditInstance.()
